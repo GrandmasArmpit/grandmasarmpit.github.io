37c256c4e950e5aa17a317475595dfbbc407dbe9
Spencer Gray <grandmasarmpit0@gmail.com>
2024-09-21 20:32:58 -0700

    update github to full overhaul (POST-HOSTING CHANGE!!)
    Just updating the github repo to allow myself to edit the website when I am not home. The website is no longer being hosted by github.

--- app.js
+++ app.js
@@ -5,19 +5,3 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
-
-const loginForm = document.getElementById("loginForm");
-
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault(); // Prevent form submission
-
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  if (username === "secret" && password === "secret") {
-    // Successful login (you can add your logic here)
-    window.location.href = "secret.html";
-  } else {
-    alert("Invalid username or password.");
-  }
-});
